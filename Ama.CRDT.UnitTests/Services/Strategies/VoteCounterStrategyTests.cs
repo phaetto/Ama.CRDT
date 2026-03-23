f7da38410c23eec459d303caead201e26ec3990f
Alexander Mantzoukas <1043745+phaetto@users.noreply.github.com>
2026-03-23 21:10:20 +0100

    Sync namespaces

--- Ama.CRDT.UnitTests/Services/Strategies/VoteCounterStrategyTests.cs
+++ Ama.CRDT.UnitTests/Services/Strategies/VoteCounterStrategyTests.cs
@@ -1,6 +1,7 @@
 namespace Ama.CRDT.UnitTests.Services.Strategies;
 
 using Ama.CRDT.Attributes;
+using Ama.CRDT.Attributes.Strategies;
 using Ama.CRDT.Extensions;
 using Ama.CRDT.Models;
 using Ama.CRDT.Models.Intents;
