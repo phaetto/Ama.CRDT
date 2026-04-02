c27ff5ef05f0ae0c23e05eb3a1349b8d9d8fd816
Alexander Mantzoukas <1043745+phaetto@users.noreply.github.com>
2026-04-02 19:33:26 +0200

    Fixes for the source generator

--- Ama.CRDT.SourceGenerators.UnitTests/CrdtContextGeneratorTests.cs
+++ Ama.CRDT.SourceGenerators.UnitTests/CrdtContextGeneratorTests.cs
@@ -70,4 +70,119 @@ namespace TestNamespace
         generatedSyntax.ShouldContain("getter: obj => ((global::TestNamespace.User)obj).Name");
         generatedSyntax.ShouldContain("setter: (obj, val) => ((global::TestNamespace.User)obj).Name = val == null ? default! : (string)val");
     }
+
+    [Fact]
+    public void Generator_ShouldNotGenerateSetter_ForInitOnlyProperties()
+    {
+        // Arrange
+        var source = @"
+using System;
+using Ama.CRDT.Attributes;
+using Ama.CRDT.Models.Aot;
+
+namespace TestNamespace
+{
+    public class ResultRecord
+    {
+        public object Document { get; init; }
+    }
+
+    [CrdtSerializable(typeof(ResultRecord))]
+    public partial class MyAotContext : CrdtContext
+    {
+    }
+}";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
+            .Select(a => MetadataReference.CreateFromFile(a.Location))
+            .Cast<MetadataReference>()
+            .ToList();
+
+        // Add reference to Ama.CRDT
+        references.Add(MetadataReference.CreateFromFile(typeof(Attributes.CrdtSerializableAttribute).Assembly.Location));
+
+        var compilation = CSharpCompilation.Create(
+            "TestCompilation",
+            [syntaxTree],
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new CrdtContextGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+        // Act
+        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+
+        // Assert
+        diagnostics.ShouldBeEmpty();
+        var runResult = driver.GetRunResult();
+        runResult.GeneratedTrees.Length.ShouldBe(1);
+
+        var generatedSyntax = runResult.GeneratedTrees[0].GetText().ToString();
+
+        // Verify that the init-only property generates a false canWrite flag and a null setter
+        generatedSyntax.ShouldContain("[\"Document\"] = new global::Ama.CRDT.Models.Aot.CrdtPropertyInfo(");
+        generatedSyntax.ShouldContain("canWrite: false");
+        generatedSyntax.ShouldContain("setter: null");
+        
+        // Assert we did not mistakenly emit an assignment lambda for Document
+        generatedSyntax.ShouldNotContain("setter: (obj, val) => ((global::TestNamespace.ResultRecord)obj).Document =");
+    }
+
+    [Fact]
+    public void Generator_ShouldNotGenerateCreateInstance_WhenClassHasRequiredMembers()
+    {
+        // Arrange
+        var source = @"
+using System;
+using Ama.CRDT.Attributes;
+using Ama.CRDT.Models.Aot;
+
+namespace TestNamespace
+{
+    public class TreeNode
+    {
+        public required string Id { get; set; }
+    }
+
+    [CrdtSerializable(typeof(TreeNode))]
+    public partial class MyAotContext : CrdtContext
+    {
+    }
+}";
+
+        var syntaxTree = CSharpSyntaxTree.ParseText(source);
+        var references = AppDomain.CurrentDomain.GetAssemblies()
+            .Where(a => !a.IsDynamic && !string.IsNullOrWhiteSpace(a.Location))
+            .Select(a => MetadataReference.CreateFromFile(a.Location))
+            .Cast<MetadataReference>()
+            .ToList();
+
+        references.Add(MetadataReference.CreateFromFile(typeof(Attributes.CrdtSerializableAttribute).Assembly.Location));
+
+        var compilation = CSharpCompilation.Create(
+            "TestCompilation",
+            [syntaxTree],
+            references,
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new CrdtContextGenerator();
+        GeneratorDriver driver = CSharpGeneratorDriver.Create(generator);
+
+        // Act
+        driver = driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var diagnostics);
+
+        // Assert
+        diagnostics.ShouldBeEmpty();
+        var runResult = driver.GetRunResult();
+        runResult.GeneratedTrees.Length.ShouldBe(1);
+
+        var generatedSyntax = runResult.GeneratedTrees[0].GetText().ToString();
+
+        // Verify that a class with a required member has a null createInstance method to avoid compile errors
+        generatedSyntax.ShouldContain("createInstance: null");
+        generatedSyntax.ShouldNotContain("createInstance: () => new global::TestNamespace.TreeNode()");
+    }
 }
